e6a5381845bc62dd402c40e0986d0deb6acdaed7
Joseph Ryan <93546701+joeyist-man@users.noreply.github.com>
2025-01-05 20:27:01 -0600

    sigma

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.LightTransport;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] animalPrefabs; // Array of GameObjects
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if  (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, 3); // Index of the animalPrefabs array
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
